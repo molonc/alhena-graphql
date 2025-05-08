6808d205e5a1f01c7bfd621b39875e231671c268
JoeMics <46177831+JoeMics@users.noreply.github.com>
2025-05-08 16:25:38 -0700

    added: updated data config constants

--- src/config.js
+++ src/config.js
@@ -1,10 +1,4 @@
 export const configConsts = [
-  {
-    label: "Autocorrelation Hmmcopy",
-    type: "autocorrelation_hmmcopy",
-    class: "numerical"
-  },
-
   { label: "Breakpoints", type: "breakpoints", class: "numerical" },
   {
     label: "Breadth Coverage Depth",
@@ -14,14 +8,6 @@ export const configConsts = [
   { label: "Cell Call", type: "cell_call", class: "categorical" },
   { label: "Column", type: "column", class: "categorical" },
   { label: "Coverage Depth", type: "coverage_depth", class: "numerical" },
-
-  { label: "Cv Hmmcopy", type: "cv_hmmcopy", class: "numerical" },
-
-  {
-    label: "Empty Bins Hmmcopy",
-    type: "empty_bins_hmmcopy",
-    class: "numerical"
-  },
   {
     label: "Estimated Library Size",
     type: "estimated_library_size",
@@ -32,59 +18,162 @@ export const configConsts = [
     type: "experimental_condition",
     class: "categorical"
   },
-
+  { 
+		label: "FastQ Screen Adapters",
+		type: "fastqscreen_adapters",
+		class: "numerical"
+	},
+  { 
+		label: "FastQ Screen Adapters Multihit",
+		type: "fastqscreen_adapters_multihit",
+		class: "numerical"
+	},
+  { 
+		label: "FastQ Screen Agrobacterium",
+		type: "fastqscreen_agrobacterium",
+		class: "numerical"
+	},
+  { 
+		label: "FastQ Screen Agrobacterium Multihit",
+		type: "fastqscreen_agrobacterium_multihit",
+		class: "numerical"
+	},
   {
-    label: "Fastqscreen Grch37",
+    label: "Fastqscreen Human",
     type: "fastqscreen_grch37",
     class: "numerical"
   },
   {
-    label: "Fastqscreen Grch37 Multihit",
+    label: "Fastqscreen Human Multihit",
     type: "fastqscreen_grch37_multihit",
     class: "numerical"
   },
-  { label: "Fastqscreen Mm10", type: "fastqscreen_mm10", class: "numerical" },
+  { 
+		label: "Fastqscreen Mouse",
+		type: "fastqscreen_mm10",
+		class: "numerical"
+	},
   {
-    label: "Fastqscreen Mm10 Multihit",
+    label: "Fastqscreen Mouse Multihit",
     type: "fastqscreen_mm10_multihit",
     class: "numerical"
   },
-  { label: "Fastqscreen Nohit", type: "fastqscreen_nohit", class: "numerical" },
+  { 
+		label: "FastQ Screen No Hit",
+		type: "fastqscreen_nohit",
+		class: "numerical"
+	},
+  { 
+		label: "FastQ Screen Pseudomonas",
+		type: "fastqscreen_pseudomonas",
+		class: "numerical"
+	},
+  { 
+		label: "FastQ Screen Pseudomonas Multihit",
+		type: "fastqscreen_pseudomonas_multihit",
+		class: "numerical"
+	},
   {
-    label: "Fastqscreen Salmon",
+    label: "FastqQ Screen Salmon",
     type: "fastqscreen_salmon",
     class: "numerical"
   },
   {
-    label: "Fastqscreen Salmon Multihit",
+    label: "FastQ Screen Salmon Multihit",
     type: "fastqscreen_salmon_multihit",
     class: "numerical"
   },
+  { 
+		label: "FastQ Screen Total Reads",
+		type: "fastqscreen_total_reads",
+		class: "numerical"
+	},
+  { 
+		label: "FastQ Screen Univec",
+		type: "fastqscreen_univec",
+		class: "numerical"
+	},
+  { 
+		label: "FastQ Screen Univec Multihit",
+		type: "fastqscreen_univec_multihit",
+		class: "numerical"
+	},
   {
-    label: "Is S Phase Probability",
+    label: "HMMcopy Autocorrelation",
+    type: "autocorrelation_hmmcopy",
+    class: "numerical"
+  },
+  { 
+		label: "HMMcopy CV",
+		type: "cv_hmmcopy",
+		class: "numerical"
+	},
+  {
+    label: "HMMcopy Empty Bins",
+    type: "empty_bins_hmmcopy",
+    class: "numerical"
+  },
+  { 
+		label: "HMMcopy Log Likelihood",
+		type: "log_likelihood",
+		class: "numerical"
+	},
+  { 
+		label: "HMMcopy MAD",
+		type: "mad_hmmcopy",
+		class: "numerical"
+	},
+  {
+    label: "HMMcopy Mean Reads Per Bin",
+    type: "mean_hmmcopy_reads_per_bin",
+    class: "numerical"
+  },
+  {
+    label: "HMMcopy Median Reads Per Bin",
+    type: "median_hmmcopy_reads_per_bin",
+    class: "numerical"
+  },
+  {
+    label: "HMMcopy StDev Reads Per Bin",
+    type: "std_hmmcopy_reads_per_bin",
+    class: "numerical"
+  },
+  {
+    label: "HMMcopy Total Mapped Reads",
+    type: "total_mapped_reads_hmmcopy",
+    class: "numerical"
+  },
+  { 
+		label: "Is Control",
+		type: "is_control",
+		class: "boolean"
+	},
+  { 
+		label: "Is S-Phase",
+		type: "is_s_phase",
+		class: "boolean"
+	},
+  {
+    label: "Is S-Phase Probability",
     type: "is_s_phase_prob",
     class: "numerical"
   },
-  { label: "Log Likelihood", type: "log_likelihood", class: "numerical" },
-  { label: "MBRSI Dispersion", type: "MBRSM_dispersion", class: "numerical" },
   {
     label: "MBRSI Dispersion Non Integerness",
     type: "MBRSI_dispersion_non_integerness",
     class: "numerical"
   },
+  { 
+		label: "MBRSM Dispersion",
+		type: "MBRSM_dispersion",
+		class: "numerical"
+	},
   {
     label: "MBRSI Non Integerness",
     type: "MBRSI_non_integerness",
     class: "numerical"
   },
-  { label: "Mad Hmmcopy", type: "mad_hmmcopy", class: "numerical" },
-  { label: "Mad Neutral State", type: "mad_neutral_state", class: "numerical" },
   { label: "Mean Copy", type: "mean_copy", class: "numerical" },
-  {
-    label: "Mean Hmmcopy Reads Per Bin",
-    type: "mean_hmmcopy_reads_per_bin",
-    class: "numerical"
-  },
   { label: "Mean Insert Size", type: "mean_insert_size", class: "numerical" },
   { label: "Mean State Mads", type: "mean_state_mads", class: "numerical" },
   { label: "Mean State Vars", type: "mean_state_vars", class: "numerical" },
@@ -94,6 +183,11 @@ export const configConsts = [
     class: "numerical"
   },
   { label: "Multiplier", type: "multiplier", class: "numerical" },
+  { 
+		label: "Neutral State MAD",
+		type: "mad_neutral_state",
+		class: "numerical" 
+	},
   {
     label: "Paired Duplicate Reads",
     type: "paired_duplicate_reads",
@@ -124,23 +218,18 @@ export const configConsts = [
   },
   { label: "State Mode", type: "state_mode", class: "categorical" },
   {
-    label: "Std Hmmcopy Reads Per Bin",
-    type: "std_hmmcopy_reads_per_bin",
-    class: "numerical"
-  },
-  {
     label: "Total Duplicate Reads",
     type: "total_duplicate_reads",
     class: "numerical"
   },
   {
-    label: "Total Mapped Reads",
-    type: "total_mapped_reads",
+    label: "Total Halfiness",
+    type: "total_halfiness",
     class: "numerical"
   },
   {
-    label: "Total Mapped Reads HMMCopy",
-    type: "total_mapped_reads_hmmcopy",
+    label: "Total Mapped Reads",
+    type: "total_mapped_reads",
     class: "numerical"
   },
   { label: "Unmapped Reads", type: "unmapped_reads", class: "numerical" },
@@ -151,6 +240,11 @@ export const configConsts = [
   },
   { label: "Total Reads", type: "total_reads", class: "numerical" },
   { label: "True Multiplier", type: "true_multiplier", class: "numerical" },
+  {
+    label: "TSS Enrichement Score",
+    type: "tss_enrichment_score",
+    class: "numerical"
+  },
   {
     label: "Unpaired Duplicate Reads",
     type: "unpaired_duplicate_reads",
